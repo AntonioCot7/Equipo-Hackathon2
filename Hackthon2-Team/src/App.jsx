e16b97d1cd8db970d5efaed98fe4789d9b5514dc
Dylan Cabezas <dylan.cabezas@utec.edu.pe>
2024-06-22 16:47:03 -0500

    Update

--- Hackthon2-Team/src/App.jsx
+++ Hackthon2-Team/src/App.jsx
@@ -17,6 +17,16 @@ function App() {
           <Route path="/Items" element={<Items />} />
           <Route path="/EditItems" element={<EditItems/>} />
           <Route path="/CreateItems" element={<CreateItems/>} />
+          <Route path="/" element={<Navigate to="/auth/login" />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
+          <PrivateRoute path="/dashboard" element={<Dashboard />} />
+          <PrivateRoute path="/editItems" element={<EditItems />} />
+          <PrivateRoute path="/createItems" element={<CreateItems />} />
+          <PrivateRoute path="/item/:id" element={<ItemDetails />} />
+          <PrivateRoute path="/cart" element={<Cart />} />
+          <PrivateRoute path="/buy" element={<Buy />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
