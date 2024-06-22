399d6ea48667c732e5f1b84d4084cec4f795f202
AntonioCot <166071640+AntonioCot7@users.noreply.github.com>
2024-06-22 17:56:13 -0500

    REGITER Y LOGIN FUNCIONA

--- Hackthon2-Team/src/App.jsx
+++ Hackthon2-Team/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import './index.css';
-
+/*
 import { Items } from './pages/Items';
 import { CreateItems } from './pages/CreateItems';
 import { EditItems } from './pages/EditItems';
@@ -12,7 +12,7 @@ import { DeleteItem } from './pages/DeleteItem';
 import { AddItemToCart } from './pages/AddItemToCart';
 import { RemoveFromCart } from './pages/RemoveFromCart';
 import { BuyCart } from './pages/BuyCart';
-
+*/
 
 
 function App() {
@@ -21,14 +21,6 @@ function App() {
     <Router>
       <div className="min-h-screen bg-gray-100">
         <Routes>
-          <Route path="/Items" element={<Items/>} />
-          <Route path="/EditItems" element={<EditItems/>} />
-          <Route path="/CreateItems" element={<CreateItems/>} />
-          <Route path="/ItemDetails" element={<ItemDetails/>} />
-          <Route path="/DeleteItem" element={<DeleteItem/>} />
-          <Route path="/AddItemCart" element={<AddItemToCart/>} />
-          <Route path="/RemoveFromCart" element={<RemoveFromCart/>} />
-          <Route path="/BuyCart" element={<BuyCart/>} />
           <Route path="/" element={<Login />} />
           <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/register" element={<Register />} />
